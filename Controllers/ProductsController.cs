c5ea6dd8d5ad85da6d90ac8e22bfcd33fed14a83
Ramadoss Elumalai <ramadosse2264@gmail.com>
2021-06-02 22:07:51 +0530

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product> ();
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            return products.Where(x=>x.Id.Equals(id)).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public Product AddProduct(Product product)
+        {
+            if(product.Id > 0) {
+                var existingProduct = products.Where(x=>x.Id.Equals(product.Id)).FirstOrDefault();
+                existingProduct.Name = product.Name;
+                existingProduct.Description = product.Description;
+                existingProduct.Category = product.Category;
+                existingProduct.Amount = product.Amount;
+            }
+            else {
+                product.Id = products.Count + 1;
+                products.Add(product);
+            }
+            return product;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteProduct(int id)
+        {
+            var product = products.Where(x=>x.Id.Equals(id)).FirstOrDefault();
+            products.Remove(product);
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProductWebApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Amount { get; set; }
+    }
+}
